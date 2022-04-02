6fc637b43409b13735bfc0db97e047aae4a115d5
Jacob Asper <jacobasper191@gmail.com>
2022-04-02 19:10:35 -0400

    Parsed percents and removed commas from csv

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
-d3
-	.select('.averageConcurrentViewers')
-	.selectAll('p')
-	.data(data)
-	.enter()
-	.append('p');
-
 const data = d3.csv('data/LearnWithLeon stream statistics - ClassData.csv').then((data) => {
 	data.forEach((x) => {
+		//change strings to numbers
+		x.Class = +x.Class
+		x['Average concurrent viewers'] = +x['Average concurrent viewers']
+		x['Peak viewers'] = +x['Peak viewers']
+
+		//remove percent sign and change to a number
+		function removePercentSign(str, property) {
+			return str[property] = Number(str[property].slice(0, str[property].length - 1))
+		}
+		removePercentSign(x, 'Percent change in average concurrent viewers ')
+		removePercentSign(x, 'Percent change in average concurrent viewers from first class')
+		removePercentSign(x, 'Percent left on average concurrent viewers from first class')
+
+		//parse date
 		x.date = new Date(String(x.date));
+
 		//convert hh:mm:ss to seconds
 		x.durationInSeconds = x.Duration.split(':').reduce((sum, newValue, i) => {
 			if (i === 0) {
@@ -23,3 +31,10 @@ const data = d3.csv('data/LearnWithLeon stream statistics - ClassData.csv').then
 	console.log(data);
 	return data;
 });
+
+// d3
+// 	.select('.averageConcurrentViewers')
+// 	.selectAll('p')
+// 	.data(data)
+// 	.enter()
+// 	.append('p');
